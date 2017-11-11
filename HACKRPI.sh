6971d99936f47495dd45dfcc3c4308222ab2564c
Kevin Ho <bornarkun@gmail.com>
2017-11-11 14:36:41 -0500

    This is the initial version of our program

---
+++ HACKRPI.sh
@@ -0,0 +1,52 @@
+clear
+
+displayMenu() {
+    echo "1) View the Top 10 Cryptocurrencies"
+    echo "2) View Your Portfolio"
+    echo "3) Buy"
+    echo "4) Sell"
+    echo "5) Exit"
+    echo ": " # Doesn't allow you to use echo -n
+}
+
+
+#WHEN BUYING HAVE THE CURRENT MARKET PRICE AND KEEP IT FLUCTUATING
+
+getAPIData() {
+	wget -qO- https://api.coinmarketcap.com/v1/ticker/?limit=10 > topCryptosData.txt # takes the top 10 cryptos and puts it into a file
+}
+
+cleanAPIData() { # gets rid of unneccesary api stuff, do i need this function?
+	echo
+}
+
+topCryptos() {
+	getAPIData	
+	#cleanAPIData
+	cat topCryptosData.txt | grep "symbol"
+}
+
+echo "Welcome to the Cryptocurrency Trading Simulator"
+echo "Please input your starting amount: " # if starting amount is less than 0 then don't let this happen
+read startingAmount
+
+while true
+do
+	displayMenu
+	read userInput
+
+	case $userInput in 
+		1) topCryptos
+		;;
+		2) echo
+		;;
+		3) echo
+		;;
+		4) echo
+		;;
+		5) echo "Goodbye!"
+			break
+		;;
+		*) echo "That is not a valid input!"
+	esac
+done
