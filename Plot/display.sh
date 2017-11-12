489b8596a0baed1c4710bc43d9c0a812d648c7a6
Dennis Hong <Dennis@Denniss-MacBook-Pro.local>
2017-11-12 12:21:59 -0500

    CLUTCH BOYES

--- Plot/display.sh
+++ Plot/display.sh
@@ -63,10 +63,45 @@ singleLine() {
 	set border lc rgb 'white'
 	set style line 1 lt 2 lw 3
 	plot 'xy$1.dat' using 1:2 notitle w l
-	EOF
+EOF
 }
 
+allLines()
+{
+	gnuplot << EOF
+	set term x11 persist background 'black'
+	set ylabel "Price ($) tc rgb 'white'
+	set xlabel "Day" tc rgb 'white'
+	set title "History" tc rgb 'white
+	set grid lc rgb 'white'
+	set border lc rgb 'white'
+	set key left bottom
+	set autoscale
 
+	set style line 1 lt 1 lc rgb "#A00000" lw 2 pt 7 ps 1.5
+	set style line 2 lt 1 lc rgb "#00A000" lw 2 pt 11 ps 1.5
+	set style line 3 lt 1 lc rgb "#5060D0" lw 2 pt 9 ps 1.5
+	set style line 4 lt 1 lc rgb "#0000A0" lw 2 pt 8 ps 1.5
+	set style line 5 lt 1 lc rgb "#D0D000" lw 2 pt 13 ps 1.5
+	set style line 6 lt 1 lc rgb "#00D0D0" lw 2 pt 12 ps 1.5
+	set style line 7 lt 1 lc rgb "#B200B2" lw 2 pt 5 ps 1.5
+	set style line 8 lt 1 lc rgb "#C9C79C" lw 2 pt 6 ps 1.5
+	set style line 9 lt 1 lc rgb "#D02090" lw 2 pt 15 ps 1.5
+	set style line 10 lt 1 lc rgb "#98BAD7" lw 2 pt 16 ps 1.5
+
+	plot "xyBTC.dat" u 1:2 lp ls 1 t "BTC" with lines,w l, \
+	"xyBCH.dat" u 1:2 lp ls 2 t "BCH" with lines, \
+	"xyETH.dat" u 1:2 lp ls 3 t "ETH" with lines, \
+	"xyXRP.dat" u 1:2 lp ls 4 t "XRP" with lines, \
+	"xyLTC.dat" u 1:2 lp ls 5 t "LTC" with lines, \
+	"xyDASH.dat" u 1:2 lp ls 6 with lines,  \
+	"xyETC.dat" u 1:2 lp ls 7 t "ETC" with lines, \
+	"xyXMR.dat" u 1:2 lp ls 8 t "XMR" with lines,  \
+	"xyNEO.dat" u 1:2 lp ls 9 t "NEO" with lines, \
+	"xyXEM.dat" u 1:2 lp ls 10 t "XEM" with lines
+EOF
+
+}
 
 display() {
 
@@ -83,75 +118,43 @@ display() {
 	read userInput
 
 	case $userInput in
-		1) fileName=$rankCoin[0]
+		1) fileName="BTC"
 			singleLine $fileName
 			;;
-		2) fileName=$rankCoin[1]
+		2) fileName="BCH"
 			singleLine $fileName
 			;;
-		3) fileName=$rankCoin[2]
+		3) fileName="ETH"
 			singeLine $fileName
 			;;
-		4) fileName=$rankCoin[3]
+		4) fileName="XRP"
 			singleLine $fileName
 			;;
-		5) fileName=$rankCoin[4]
+		5) fileName="LTC"
 			singleLine $fileName
 			;;
-		6) fileName=$rankCoin[5]
+		6) fileName="DASH"
 			singleLine $fileName
 			;;
-		7) fileName=$rankCoin[6]
+		7) fileName="ETC"
 			singleLine $fileName
 			;;
-		8) fileName=$rankCoin[7]
+		8) fileName="XMR"
 			singleLine $fileName
 			;;
-		9) fileName=$rankCoin[8]
+		9) fileName="NEO"
 			singleLine $fileName
 			;;
-		10) fileName=$rankCoin[9]
+		10) fileName="XEM"
 			singleLine $fileName
 			;;
-		11) gnuplot << EOF
-			set term x11 persist background 'black'
-			set ylabel "Price ($) tc rgb 'white'
-			set xLabel "Day" tc rgb 'white'
-			set title "History" tc rgb 'white
-			set grid lc rgb 'white'
-			set border lc rgb 'white'
-			set key left bottom
-		
-			set autoscale
-
-			set style line 1 lt 1 lc rgb "#A00000" lw 2 pt 7 ps 1.5
-			set style line 2 lt 1 lc rgb "#00A000" lw 2 pt 11 ps 1.5
-			set style line 3 lt 1 lc rgb "#5060D0" lw 2 pt 9 ps 1.5
-			set style line 4 lt 1 lc rgb "#0000A0" lw 2 pt 8 ps 1.5
-			set style line 5 lt 1 lc rgb "#D0D000" lw 2 pt 13 ps 1.5
-			set style line 6 lt 1 lc rgb "#00D0D0" lw 2 pt 12 ps 1.5
-			set style line 7 lt 1 lc rgb "#B200B2" lw 2 pt 5 ps 1.5
-			set style line 8 lt 1 lc rgb "#A3D021" lw 2 pt 14 ps 1.5
-			set style line 8 lt 1 lc rgb "FF13ASD" lw 2 pt 6 ps 1.5
-			set style line 9 lt 1 lc rgb "4444444" lw 2 pt 15 ps 1.5
-			set style line 10 lt 1 lc rgb "9932000" lw 2 pt 16 ps 1.5
-
-			plot "xy$rankCoin[0].dat" u 1:2 lp ls 1 t "$rankCoin[0]" w l, 
-			"xy$rankCoin[1].dat" u 1:2 lp ls 2 t "$rankCoin[1]" w l, 
-			"xy$rankCoin[2].dat" u 1:2 lp ls 3 t "$rankCoin[2]" w l,
-			"xy$rankCoin[3].dat" u 1:2 lp ls 4 t "$rankCoin[3]" w l,
-			"xy$rankCoin[4].dat" u 1:2 lp ls 5 t "$rankCoin[4]" w l,
-			"xy$rankCoin[5].dat" u 1:2 lp ls 6 t "$rankCoin[5]" w l,
-			"xy$rankCoin[6].dat" u 1:2 lp ls 7 t "$rankCoin[6]" w l,
-			"xy$rankCoin[7].dat" u 1:2 lp ls 8 t "$rankCoin[7]" w l,
-			"xy$rankCoin[8].dat" u 1:2 lp ls 9 t "$rankCoin[8]" w l,
-			"xy$rankCoin[9].dat" u 1:2 lp ls 10 t "$rankCoin[9]" w l
-
-			;;
+		11) allLines
+		;;
 		12) echo "Update Top Ten History"
 			grabHistory
 			;;
 		*) echo "Invalid Input"
+		break
 	esac
 
 }
